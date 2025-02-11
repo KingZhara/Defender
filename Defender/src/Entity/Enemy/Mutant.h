a5632146aa68ca25d6fce4437bf7b2b1aa3d61d2
KingZhara <waroperationplannedresponse@protonmail.com>
2025-02-11 12:02:50 -0600

    A

--- Defender/src/Entity/Enemy/Mutant.h
+++ Defender/src/Entity/Enemy/Mutant.h
@@ -11,6 +11,8 @@ class Mutant :
     */
     const uint16_t xp = 150;
 
+    //https://en.wikipedia.org/wiki/List_of_software_palettes#/media/File:RGB_6levels_palette_color_test_chart.png
+
     virtual bool tick();
 };
 
