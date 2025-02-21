897e979fcee041bd3288cec7ca61c8e529fee4a0
ComdrBubbles <elliottwilcox007@gmail.com>
2025-02-21 11:55:16 -0600

    Entity animation data

--- Defender/src/Entity/Source/Entity.cpp
+++ Defender/src/Entity/Source/Entity.cpp
@@ -4,30 +4,51 @@
 const Entity::SpriteData Entity::SPRITE_TABLE[EntityID::LENGTH] =
 {
 	{ // Player
-		{}, // Sprite IntRect
-		{}, // Sprite frame count
+		{0,16,15,6}, // Sprite IntRect
+		2, // Sprite frame count
 		ShaderID::NONE, // Sprite shader (Optional parameter)
-		1./15., // Sprite frame length (Optional parameter, double)
+		1. / 15., // Sprite frame length (Optional parameter, double)
 	},
 	{ // Astronaut
+		{42, 0, 3, 8},
+		0
 	},
 	{ // Bullet
+		{8,8,3,3},
+		1
 	},
 	{ // Laser
+		// @todo Replace later; blank data, will not use a sprite for drawing, custom image
+		{19,0,8,8},
+		2
 	},
 	{ // Bomb
+		{19,0,8,8},
+		2
 	},
 	{ // Lander
+		{19,8,9,8},
+		3
 	},
 	{ // Mutant
+		{0,0,10,8},
+		1
 	},
 	{ // Baiter
+		{55,8,11,4},
+		2
 	},
 	{ // Bomber
+		{45,0,6,7},
+		3
 	},
 	{ // Pod
+		{35,0,7,7},
+		0
 	},
 	{ // Swarmer
+		{14,8,5,4},
+		0
 	}
 };
 
