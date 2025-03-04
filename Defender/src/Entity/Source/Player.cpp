acf8ebb22caffd41acc7b18175dcbbf2a1fcc638
KingZhara <waroperationplannedresponse@protonmail.com>
2025-03-04 10:50:22 -0600

    *

--- Defender/src/Entity/Source/Player.cpp
+++ Defender/src/Entity/Source/Player.cpp
@@ -2,22 +2,21 @@
 
 void Player::setActions(Action& actions)
 {
-	sf::IntRect texRect = animation.getSprite().getTextureRect();
+	//sf::IntRect texRect = animation.getSprite().getTextureRect();
 
 	this->actions = actions;
 	if (actions.flags.left)
 	{
 		left = true;
 
-		texRect.left = 0;
+		animation.setStart(0);
 	}
 	else if (actions.flags.right)
 	{
 		left = false;
 
-		texRect.left = SPRITE_TABLE[EntityID::PLAYER].bounds.left * 2;
 	}
 
 
-	animation.getSprite().setTextureRect(texRect);
+	//animation.getSprite().setTextureRect(texRect);
 }
