23bf3dd135f438e0a751fb2a2b10212c56243746
ComdrBubbles <elliottwilcox007@gmail.com>
2025-03-12 12:05:07 -0500

    Mutant ai

--- Defender/src/Entity/Source/Enemy.cpp
+++ Defender/src/Entity/Source/Enemy.cpp
@@ -2,15 +2,38 @@
 
 void Enemy::tick(double deltatime)
 {
-	if (playerPos->x < pos.x)
-		vel.x = -0.5f;
-	else if (playerPos->x > pos.x)
-		vel.x = 0.5f;
+	bool attackMode = false;
 
-	if (playerPos->y < pos.y)
-		vel.y = -0.5f;
-	else if (playerPos->y > pos.y)
-		vel.y = 0.5f;
+	if (getID() == EntityID::MUTANT) 
+	{
+		if (abs(playerPos->x-pos.x)<70) 
+		{
+			if (playerPos->y < pos.y)
+				vel.y = -0.5f;
+			if (playerPos->y > pos.y)
+				vel.y = 0.5f;
+		}
+		else 
+		{
+			vel.x = 0.5f;
+			vel.y = 0;
+		}
 
-	Entity::tick(deltatime);
+		Entity::tick(deltatime);
+	}
+
+	if (getID() == EntityID::LANDER)
+	{
+		if (playerPos->x < pos.x)
+			vel.x = -0.5f;
+		else if (playerPos->x > pos.x)
+			vel.x = 0.5f;
+
+		if (playerPos->y < pos.y)
+			vel.y = -0.5f;
+		else if (playerPos->y > pos.y)
+			vel.y = 0.5f;
+
+		Entity::tick(deltatime);
+	}
 }
--- Defender/src/State/Source/StageState.cpp
+++ Defender/src/State/Source/StageState.cpp
@@ -12,7 +12,7 @@ StageState::StageState()
 	entityManager.spawn(EntityManager::SpawnType::PLAYER, { 250, 150 }, EntityID::PLAYER);
 	entityManager.spawn(EntityManager::SpawnType::ASTRONAUT, { 50, 50 }, EntityID::ASTRONAUT);
 	entityManager.spawn(EntityManager::SpawnType::ENEMY, { 50, 50 }, EntityID::MUTANT);
-	entityManager.spawn(EntityManager::SpawnType::ENEMY, { 50, 50 }, EntityID::MUTANT);
+	//entityManager.spawn(EntityManager::SpawnType::ENEMY, { 50, 100 }, EntityID::LANDER);
 	entityManager.spawn(EntityManager::SpawnType::ENEMY, { 50, 50 }, EntityID::MUTANT);
 }
 
