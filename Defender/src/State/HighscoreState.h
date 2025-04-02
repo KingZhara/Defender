394dc37d29211eb9568193eaebc2d70f2c60bd58
idk-v1 <benhamilton.jpg@gmail.com>
2025-04-01 19:57:46 -0500

    decorated highscore page, defaults only

--- Defender/src/State/HighscoreState.h
+++ Defender/src/State/HighscoreState.h
@@ -14,7 +14,41 @@ public:
 
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 	{
-		target.clear(sf::Color::Red);
+		target.draw(defender, states);
+
+		target.draw(hallOfFame, states);
+
+		target.draw(goatodayTitle, states);
+		target.draw(goatimeTitle, states);
+
+		target.draw(goatodayUnder, states);
+		target.draw(goatimeUnder, states);
+
+		target.draw(goatoday);
+		target.draw(goatime);
 	}
+
+private:
+
+	struct Score
+	{
+		char initials[3] = { 'A', 'B', 'C' };
+		int score = 12345;
+	};
+
+	std::string padNum(int num, int length);
+
+	std::string makeScores(Score scores[8]);
+
+	Score today[8], allTime[8];
+
+	sf::Text goatoday, goatime, goatodayTitle, goatimeTitle, hallOfFame;
+
+	sf::RectangleShape goatodayUnder, goatimeUnder;
+
+	sf::Texture defenderTex;
+	sf::RectangleShape defender;
+
+	sf::Color lime = sf::Color(0x89FF00FF);
 };
 
--- Defender/src/State/Source/HighscoreState.cpp
+++ Defender/src/State/Source/HighscoreState.cpp
@@ -1,7 +1,51 @@
 #include "../HighscoreState.h"
+#include "../../Utility/common.h"
+#include "../../Utility/UserInterface/UserInterface.h"
 
 HighscoreState::HighscoreState()
 {
+	defenderTex.loadFromFile("res/defender.png");
+	defender.setSize(sf::Vector2f(defenderTex.getSize()) / 2.f);
+	defender.setTexture(&defenderTex);
+	defender.setPosition(COMN::resolution.x / 2 - defender.getGlobalBounds().getSize().x / 2, 30);
+
+	hallOfFame.setFont(UserInterface::getFont());
+	hallOfFame.setString("HALL OF FAME");
+	hallOfFame.setCharacterSize(16);
+	hallOfFame.setPosition(COMN::resolution.x / 2 - (18 * 12.75f / 4), 50);
+	hallOfFame.setFillColor(lime);
+
+	goatodayTitle.setFont(UserInterface::getFont());
+	goatodayTitle.setString(" TODAYS \nGREATEST");
+	goatodayTitle.setCharacterSize(16);
+	goatodayTitle.setPosition(COMN::resolution.x / 4 - (18 * 8.f / 4), 80);
+	goatodayTitle.setFillColor(lime);
+
+	goatimeTitle.setFont(UserInterface::getFont());
+	goatimeTitle.setString("ALL TIME\nGREATEST");
+	goatimeTitle.setCharacterSize(16);
+	goatimeTitle.setPosition(COMN::resolution.x / 4 * 3 - (18 * 8.f / 4), 80);
+	goatimeTitle.setFillColor(lime);
+
+	goatodayUnder.setSize(sf::Vector2f(18 * 8.5f / 2, 2));
+	goatodayUnder.setPosition(COMN::resolution.x / 4 - (18 * 8.f / 4), 110);
+	goatodayUnder.setFillColor(lime);
+
+	goatimeUnder.setSize(sf::Vector2f(18 * 8.5f / 2, 2));
+	goatimeUnder.setPosition(COMN::resolution.x / 4 * 3 - (18 * 8.f / 4), 110);
+	goatimeUnder.setFillColor(lime);
+
+	goatoday.setFont(UserInterface::getFont());
+	goatoday.setString(makeScores(today));
+	goatoday.setCharacterSize(16);
+	goatoday.setPosition(COMN::resolution.x / 4 - (18 * 10.f / 4), 110);
+	goatoday.setFillColor(lime);
+
+	goatime.setFont(UserInterface::getFont());
+	goatime.setString(makeScores(allTime));
+	goatime.setCharacterSize(16);
+	goatime.setPosition(COMN::resolution.x / 4 * 3 - (18 * 10.f / 4), 110);
+	goatime.setFillColor(lime);
 }
 
 HighscoreState::~HighscoreState()
@@ -11,4 +55,26 @@ HighscoreState::~HighscoreState()
 bool HighscoreState::tick(double deltatime)
 {
 	return false;
+}
+
+std::string HighscoreState::padNum(int num, int length)
+{
+	std::string str(length, 0);
+	snprintf(str.data(), length + 1, "%*d", length, num);
+	return str;
+}
+
+std::string HighscoreState::makeScores(Score scores[8])
+{
+	std::string str;
+	for (int i = 0; i < 8; i++)
+	{
+		str += ('1' + i);
+		str += scores[i].initials[0];
+		str += scores[i].initials[1];
+		str += scores[i].initials[2];
+		str += padNum(scores[i].score, 6);
+		str += '\n';
+	}
+	return str;
 }
