c64f84a06097d7b3122d39757787451f4d68a876
ComdrBubbles <elliottwilcox007@gmail.com>
2025-04-21 11:01:37 -0500

    POD

--- Defender/src/Entity/Enemy/Source/Pod.cpp
+++ Defender/src/Entity/Enemy/Source/Pod.cpp
@@ -2,16 +2,6 @@
 
 void Pod::tick(double deltatime)
 {
-	if (rot > 180)
-		rot -= 360;
-
-	if (rot < -45 && rot > -135)
-		rot = 45;
-
-	if (rot > 45 && rot < 135)
-		rot = 135;
-
-	std::cout << rot << '\n';
 
 	vel.x = (float)(43 * cos(rot*(3.141592653589/180)));
 	vel.y = (float)(43 * sin(rot*(3.141592653589/180)));
