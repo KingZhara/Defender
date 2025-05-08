2a099ed51e729f098c9196e310c0d604a18e43f4
idk-v1 <benhamilton.jpg@gmail.com>
2025-05-08 14:16:15 -0500

    lasers work

--- Defender/src/Entity/Projectile/Laser.h
+++ Defender/src/Entity/Projectile/Laser.h
@@ -9,27 +9,43 @@ public:
         : Projectile(pos_, EntityID::LASER, isScripted_, script_)
     {
         shifting.create(COMN::resolution.x, 1);
-        laser.create(COMN::resolution.x, 1);
-        shftDra.setTexture(shifting);
+        shftDra.setTexture(shifting.getTexture());
+        shftDra.setPosition(getPos());
+
+        laserTex.create(COMN::resolution.x, 1);
+        laserNoise.create(COMN::resolution.x, 1, sf::Color(0));
+        laserTex.setRepeated(true);
 
         // Laser beam randomness doesn't move with the laser
-        for (int x = 0; x < laser.getSize().x; x++)
-        {
+        for (int x = 0; x < laserNoise.getSize().x; x++)
             if (rand() % 2)
-                laser.setPixel(x, 0, sf::Color(136, 0, 255));
-        }
+                laserNoise.setPixel(x, 0, sf::Color::White);
+        laserTex.loadFromImage(laserNoise);
+
+
+        head = tail = 0;
     }
 
     void tick(double deltatime)
     {
         // update textures here
 
-        shifting.update(laser);
+        // Sorta magic numbers sorry
+        // The head moves 2x faster than the tail
+        head += abs(vel.x) * deltatime * 1.333f;
+        tail += abs(vel.x) * deltatime * 0.667f;
 
-        shftDra.setPosition(getPos());
+        if (vel.x < 0)
+        {
+            shftDra.setScale(-1, 1);
+            shftDra.setPosition(getPos().x + (head - tail), getPos().y + 3);
+        }
+        else
+        {
+            shftDra.setPosition(getPos().x - (head - tail), getPos().y + 3);
+        }
 
-
-        Entity::tick(deltatime);
+        Projectile::tick(deltatime);
     }
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override
@@ -48,16 +64,46 @@ public:
 
         UserInterface::getShader(ShaderID::HUE_SHIFT)->setUniform("texture", sf::Shader::CurrentTexture);
 
+        shifting.clear();
+
+        // Random back 2/3 of laser
+        sf::RectangleShape laserRand;
+        laserRand.setFillColor(sf::Color(136, 0, 255));
+        laserRand.setSize(sf::Vector2f((head - tail) * 0.667f, 1));
+        laserRand.setPosition(0, 0);
+        laserRand.setTexture(&laserTex);
+        // scroll texture back to undo movement
+        laserRand.setTextureRect(sf::IntRect(-getPos().x + (head * 0.667f + tail), 
+            0, (head - tail) * 0.667f, 1));
+        shifting.draw(laserRand, states);
+
+
+        // Soid 1/3 of laser
+        sf::RectangleShape laserSolid;
+        laserSolid.setFillColor(sf::Color(136, 0, 255));
+        laserSolid.setSize(sf::Vector2f((head - tail) * 0.333f, 1));
+        laserSolid.setPosition((head - tail) * 0.667f, 0);
+        shifting.draw(laserSolid, states);
+
+
+        sf::RectangleShape laserHead;
+        laserHead.setPosition(head - tail, 0);
+        laserHead.setSize({ 2, 1 });
+        shifting.draw(laserHead, states);
+
+
         states.shader = UserInterface::getShader(ShaderID::HUE_SHIFT);
+
         target.draw(shftDra, states);
     }
 
 private:
 
-    sf::Texture shifting;
-    sf::Image laser;
+    mutable sf::RenderTexture shifting;
+    sf::Texture laserTex;
+    sf::Image laserNoise;
     sf::Sprite shftDra;
 
-    float start = 0, end = 0;
+    float head = 0, tail = 0;
 };
 
